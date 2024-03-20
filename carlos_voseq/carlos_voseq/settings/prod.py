558a98b3d663e88e3dc2fb941d8395c0df40981b
Carlos Peña <mycalesis@gmail.com>
2024-03-20 16:56:29 -0500

    fix path

--- carlos_voseq/carlos_voseq/settings/prod.py
+++ carlos_voseq/carlos_voseq/settings/prod.py
@@ -5,6 +5,14 @@ import environ
 from .base import *  # noqa
 
 DEBUG = False
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '..', '.env'))
 env = environ.Env()
+
 SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = ["carlos.voseq.rocks"]
