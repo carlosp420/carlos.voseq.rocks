c7f5193c2c58d5d5a3e380554e40abbfad364330
Carlos Peña <mycalesis@gmail.com>
2024-03-20 22:10:30 -0500

    append slash to end or url

--- carlos_voseq/home/urls.py
+++ carlos_voseq/home/urls.py
@@ -4,6 +4,6 @@ from home import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("portfolio", views.portfolio, name="portfolio"),
-    path("portfolio/manolo", views.manolo, name="manolo"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path("portfolio/manolo/", views.manolo, name="manolo"),
 ]
